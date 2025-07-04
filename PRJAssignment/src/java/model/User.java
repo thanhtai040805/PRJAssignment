94a844d0db616077cf664d188fa39cc235e40bc4
DELL <DELL@PhamTanHai.lan>
2025-07-04 11:38:03 +0700

    Fix Model/ Add Mail

--- PRJAssignment/src/java/model/User.java
+++ PRJAssignment/src/java/model/User.java
@@ -1,29 +1,35 @@
 package model;
 
-import java.util.Date;
+import java.util.Date; // Keep if ngaySinh still uses it, or consider migrating ngaySinh to LocalDate as well
+import model.Customer; // Make sure this is correctly imported
+import model.Employee; // Make sure this is correctly imported
+import model.AdminProfile; // Make sure this is correctly imported
+import java.time.LocalDateTime; // This is crucial
 
 public class User {
+
     private int maUser;
     private String username;
     private String password;
     private String email;
     private String hoTen;
-    private Date ngaySinh;
+    private Date ngaySinh; // Sticking with java.util.Date for now based on your code
     private String gioiTinh;
     private String diaChi;
     private String soDienThoai;
     private String cccd;
     private String role;
-    private Date ngayTao;
+    private LocalDateTime ngayTao; // THIS IS CORRECT - keep it as LocalDateTime
     private String trangThai;
-    
+
     // Profile information
-    private CustomerProfile customerProfile;
-    private EmployeeProfile employeeProfile;
+    private Customer customer;
+    private Employee employee; // Renamed from employeeProfile for consistency with getter
     private AdminProfile adminProfile;
 
     // Constructors
-    public User() {}
+    public User() {
+    }
 
     public User(String username, String password, String email, String hoTen, String role) {
         this.username = username;
@@ -31,8 +37,31 @@ public class User {
         this.email = email;
         this.hoTen = hoTen;
         this.role = role;
-        this.ngayTao = new Date();
+        this.ngayTao = LocalDateTime.now(); // FIX: Use LocalDateTime.now()
         this.trangThai = "Hoạt động";
+    }
+
+    // Full constructor (if needed, useful for loading from DB)
+    // IMPORTANT: If you pass a java.util.Date for ngayTao here, it will be an issue
+    // for LocalDateTime field. You should align all dates to either java.util.Date
+    // or java.time.* for consistency.
+    // For now, I'm adapting this constructor to accept LocalDateTime for ngayTao
+    public User(int maUser, String username, String password, String email, String hoTen, Date ngaySinh,
+            String gioiTinh, String diaChi, String soDienThoai, String cccd, String role,
+            LocalDateTime ngayTao, String trangThai) { // FIX: Change Date to LocalDateTime for ngayTao parameter
+        this.maUser = maUser;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.hoTen = hoTen;
+        this.ngaySinh = ngaySinh;
+        this.gioiTinh = gioiTinh;
+        this.diaChi = diaChi;
+        this.soDienThoai = soDienThoai;
+        this.cccd = cccd;
+        this.role = role;
+        this.ngayTao = ngayTao; // This assignment is now correct
+        this.trangThai = trangThai;
     }
 
     // Getters and Setters
@@ -124,11 +153,11 @@ public class User {
         this.role = role;
     }
 
-    public Date getNgayTao() {
+    public LocalDateTime getNgayTao() { // THIS IS CORRECT
         return ngayTao;
     }
 
-    public void setNgayTao(Date ngayTao) {
+    public void setNgayTao(LocalDateTime ngayTao) { // FIX: Change parameter type from Date to LocalDateTime
         this.ngayTao = ngayTao;
     }
 
@@ -140,20 +169,21 @@ public class User {
         this.trangThai = trangThai;
     }
 
-    public CustomerProfile getCustomerProfile() {
-        return customerProfile;
+    // Getters and Setters cho Customer
+    public Customer getCustomer() {
+        return customer;
     }
 
-    public void setCustomerProfile(CustomerProfile customerProfile) {
-        this.customerProfile = customerProfile;
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
     }
 
-    public EmployeeProfile getEmployeeProfile() {
-        return employeeProfile;
+    public Employee getEmployee() { // Changed to getEmployeeProfile for consistency with DAO
+        return employee;
     }
 
-    public void setEmployeeProfile(EmployeeProfile employeeProfile) {
-        this.employeeProfile = employeeProfile;
+    public void setEmployee(Employee employeeProfile) { // Changed to setEmployeeProfile for consistency with DAO
+        this.employee = employeeProfile;
     }
 
     public AdminProfile getAdminProfile() {
@@ -166,30 +196,30 @@ public class User {
 
     // Utility methods
     public boolean isAdmin() {
-        return "ADMIN".equals(this.role);
+        return "ADMIN".equalsIgnoreCase(this.role);
     }
 
     public boolean isEmployee() {
-        return "EMPLOYEE".equals(this.role);
+        return "EMPLOYEE".equalsIgnoreCase(this.role);
     }
 
     public boolean isCustomer() {
-        return "CUSTOMER".equals(this.role);
+        return "CUSTOMER".equalsIgnoreCase(this.role);
     }
 
     public boolean isActive() {
-        return "Hoạt động".equals(this.trangThai);
+        return "Hoạt động".equalsIgnoreCase(this.trangThai);
     }
 
     @Override
     public String toString() {
-        return "User{" +
-                "maUser=" + maUser +
-                ", username='" + username + '\'' +
-                ", email='" + email + '\'' +
-                ", hoTen='" + hoTen + '\'' +
-                ", role='" + role + '\'' +
-                ", trangThai='" + trangThai + '\'' +
-                '}';
+        return "User{"
+                + "maUser=" + maUser
+                + ", username='" + username + '\''
+                + ", email='" + email + '\''
+                + ", hoTen='" + hoTen + '\''
+                + ", role='" + role + '\''
+                + ", trangThai='" + trangThai + '\''
+                + '}';
     }
 }
